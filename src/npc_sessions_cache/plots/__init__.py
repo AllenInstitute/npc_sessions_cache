471e7180d22837f94274d274449154dd27841abb
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-07-26 12:01:37 -0700

    Add metadata to qc

--- src/npc_sessions_cache/plots/__init__.py
+++ src/npc_sessions_cache/plots/__init__.py
@@ -4,3 +4,4 @@ from npc_sessions_cache.plots.spikes import *
 from npc_sessions_cache.plots.sync import *
 from npc_sessions_cache.plots.timing import *
 from npc_sessions_cache.plots.video import *
+from npc_sessions_cache.plots._metadata import *

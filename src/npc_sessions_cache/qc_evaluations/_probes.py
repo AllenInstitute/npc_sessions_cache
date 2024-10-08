46e37a6835315bf72908c6c8b77eb270b624350f
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-10-08 16:58:20 -0700

    Move & rename ccf qc plots

--- src/npc_sessions_cache/qc_evaluations/_probes.py
+++ src/npc_sessions_cache/qc_evaluations/_probes.py
@@ -4,10 +4,7 @@ from npc_sessions_cache.plots.ephys import (
     plot_sorting_view_summary_links,
     plot_sorting_view_timeseries_links,
     plot_unit_metrics,
-    plot_ephys_noise_with_unit_density_ccf_areas,
-    plot_ccf_electrodes_implant_hole,
 )
-
 
 instructions = {
     plot_probe_yield: """
@@ -19,9 +16,5 @@ instructions = {
     plot_sorting_view_timeseries_links: """
     """,
     plot_sorting_view_summary_links: """
-    """,
-    plot_ephys_noise_with_unit_density_ccf_areas: """
-    """,
-    plot_ccf_electrodes_implant_hole: """
     """,
 }

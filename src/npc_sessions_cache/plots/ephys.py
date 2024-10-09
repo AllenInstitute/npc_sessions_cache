c77d0af04a7a9624833a007eed78ffbf8c0bbd43
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-10-09 09:02:58 -0700

    Adjust qc plots

--- src/npc_sessions_cache/plots/ephys.py
+++ src/npc_sessions_cache/plots/ephys.py
@@ -1080,17 +1080,21 @@ def plot_sensory_responses(
             {
                 "unit_id": unit_id,
                 "vis_resp": np.median(
-                    v := np.subtract(block_resp["vis"], block_resp["aud"])
-                ) * (max(v) / min(v)),
+                    (v := np.subtract(block_resp["vis"], block_resp["aud"]))
+                    - (max(v) - min(v))
+                ),
                 "aud_resp": np.median(
-                    v := np.subtract(block_resp["aud"], block_resp["vis"])
-                ) * (max(v) / min(v)),
+                    (v := np.subtract(block_resp["aud"], block_resp["vis"]))
+                    - (max(v) - min(v))
+                ),
                 "stim_resp": np.median(
-                    v := np.subtract(
-                        np.add(block_resp["aud"], block_resp["vis"]) * 0.5,
-                        block_resp["catch"],
-                    )
-                ) * (max(v) / min(v)),
+                    (
+                        v := np.subtract(
+                            np.add(block_resp["aud"], block_resp["vis"]) * 0.5,
+                            block_resp["catch"],
+                        )
+                    ) - (max(v) - min(v))
+                ),
             }
         )
     stim_resp_df = pd.DataFrame(records)

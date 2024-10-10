6ee892d7dfc70c5df46c6f82ab0d72d0aee291fb
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-10-10 11:17:54 -0700

    Add surface images

--- src/npc_sessions_cache/plots/__init__.py
+++ src/npc_sessions_cache/plots/__init__.py
@@ -5,3 +5,4 @@ from npc_sessions_cache.plots.sync import *
 from npc_sessions_cache.plots.timing import *
 from npc_sessions_cache.plots.video import *
 from npc_sessions_cache.plots._metadata import *
+from npc_sessions_cache.plots.health import *

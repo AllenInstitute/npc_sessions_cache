1bf97bb8b037c83c0f9e0e863b974dae4a0bdfe2
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-10-11 17:43:38 -0700

    Fix probe qc

--- src/npc_sessions_cache/qc_evaluations/2_probeF.py
+++ src/npc_sessions_cache/qc_evaluations/2_probeF.py
@@ -4,6 +4,7 @@ import pathlib
 import npc_session
 from npc_sessions_cache.qc_evaluations._probes import *
 
-for name, func in globals().items():
+for name in tuple(globals().keys()):
+    func = globals()[name]
     if name.startswith('plot_') and callable(func):
         globals()[name] = functools.partial(func, probe_letter=npc_session.ProbeRecord(pathlib.Path(__file__).stem))

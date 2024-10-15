df83e609bb52e2f4900bcad18b335e8ec266011f
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-10-15 16:01:19 -0700

    Add missing probe annotation qc

--- src/npc_sessions_cache/qc_evaluations/3_probes_missing.py
+++ src/npc_sessions_cache/qc_evaluations/3_probes_missing.py
@@ -17,3 +17,13 @@ def plot_probes_skipped_in_config(session: npc_sessions.DynamicRoutingSession) -
     if not session.info:
         return None
     return ''.join(sorted(session.info.session_kwargs.get('probe_letters_to_skip', '')))
+
+def plot_probes_missing_from_annotations(session: npc_sessions.DynamicRoutingSession) -> str | None:
+    if not session.is_ephys:
+        return None
+    return ''.join(sorted(set('ABCDEF').difference(npc_sessions.get_tissuecyte_electrodes_table(session.id).group_name.unique())))
+
+def plot_insertion_info(session: npc_sessions.DynamicRoutingSession) -> dict | None:
+    if not session.is_ephys:
+        return None
+    return session.probe_insertion_info

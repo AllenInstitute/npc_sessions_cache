bf6aa246a41ada8d388d2ec1330d4e59e5c8ba11
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-10-15 18:11:59 -0700

    Fix aud qc

--- src/npc_sessions_cache/qc_evaluations/1_aud_sync.py
+++ src/npc_sessions_cache/qc_evaluations/1_aud_sync.py
@@ -1,8 +1,8 @@
 import functools
 
-from npc_sessions_cache.plots.audio import plot_microphone_response, plot_audio_waveforms
-plot_audio_target_waveforms = functools.partial(plot_audio_waveforms, target_stim=True)
-plot_audio_nontarget_waveforms = functools.partial(plot_audio_waveforms, target_stim=False)
+from npc_sessions_cache.plots.audio import plot_microphone_response, plot_audio_waveforms as _plot_audio_waveforms
+plot_audio_target_waveforms = functools.partial(_plot_audio_waveforms, target_stim=True)
+plot_audio_nontarget_waveforms = functools.partial(_plot_audio_waveforms, target_stim=False)
 
 instructions = {
     plot_microphone_response: """
